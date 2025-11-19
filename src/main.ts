8691c4a903022d70d6a0cacbfe67c8e9d30d0dab
Zuzzang2 <mandy021021@gmail.com>
2025-11-20 00:47:45 +0900

    feat: 전역 ValidationPipe 및 CORS 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  // CORS 설정
+  app.enableCors({
+    origin: 'http://localhost:5173/',
+    credentials: true,
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
